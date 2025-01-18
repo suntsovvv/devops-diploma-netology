2c773cfb06e9b06925c35b99edc05fc2c7f1f90b
suntsovvv <suntsovvv@gmail.com>
2025-01-18 15:30:01 +0000

    2

--- terraform/auto_inventory.tf
+++ terraform/auto_inventory.tf
@@ -1,9 +1,9 @@
-resource "local_file" "hosts_cfg_kubespray" {
+resource "local_file" "hosts_cfg" {
 #  count = var.exclude_ansible ? 0 : 1
   content = templatefile("inventory.tftpl",
     { 
-      masters = yandex_compute_instance.master
-      workers = yandex_compute_instance.worker
+      k8s-masters = yandex_compute_instance.master
+      k8s-workers = yandex_compute_instance.worker
     })
 
   filename = "../ansible/hosts.yaml"
